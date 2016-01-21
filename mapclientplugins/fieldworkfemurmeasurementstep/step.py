dfecd219fcfde0e3c96f68282a0af439f88fe86f
juzhang <jzha263@aucklanduni.ac.nz>
2016-01-21 21:09:19 +1300

    use GIAS2

--- mapclientplugins/fieldworkfemurmeasurementstep/step.py
+++ mapclientplugins/fieldworkfemurmeasurementstep/step.py
@@ -1,119 +1,3 @@
-<<<<<<< HEAD
-'''
-MAP Client, a program to generate detailed musculoskeletal models for OpenSim.
-    Copyright (C) 2012  University of Auckland
-    
-This file is part of MAP Client. (http://launchpad.net/mapclient)
-
-    MAP Client is free software: you can redistribute it and/or modify
-    it under the terms of the GNU General Public License as published by
-    the Free Software Foundation, either version 3 of the License, or
-    (at your option) any later version.
-
-    MAP Client is distributed in the hope that it will be useful,
-    but WITHOUT ANY WARRANTY; without even the implied warranty of
-    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-    GNU General Public License for more details.
-
-    You should have received a copy of the GNU General Public License
-    along with MAP Client.  If not, see <http://www.gnu.org/licenses/>..
-'''
-import os
-
-from PySide import QtCore, QtGui
-
-from mapclient.mountpoints.workflowstep import WorkflowStepMountPoint
-
-from mapclientplugins.fieldworkfemurmeasurementstep.widgets.configuredialog import ConfigureDialog
-from mapclientplugins.fieldworkfemurmeasurementstep.fieldworkfemurmeasurementdata import StepState
-from gias2.musculoskeletal import fw_femur_measurements
-from gias2.fieldwork.field import geometric_field
-
-class FieldworkFemurMeasurementStep(WorkflowStepMountPoint):
-    '''
-    Take morphometric measurements on a fieldwork femur mesh
-    '''
-    
-    def __init__(self, location):
-        super(FieldworkFemurMeasurementStep, self).__init__('Fieldwork Femur Measurements', location)
-        self._category = 'Fieldwork Measurements'
-        self._state = StepState()
-        # self._icon = QtGui.QImage(':/zincmodelsource/images/zinc_model_icon.png')   # change this
-        self.addPort(('http://physiomeproject.org/workflow/1.0/rdf-schema#port',
-                      'http://physiomeproject.org/workflow/1.0/rdf-schema#uses',
-                      'ju#fieldworkmodel'))
-        # self.addPort(('http://physiomeproject.org/workflow/1.0/rdf-schema#port', 'http://physiomeproject.org/workflow/1.0/rdf-schema#provides', 'ju#fieldworkfemurmeasurement'))
-        self.addPort(('http://physiomeproject.org/workflow/1.0/rdf-schema#port',
-                      'http://physiomeproject.org/workflow/1.0/rdf-schema#provides',
-                      'ju#fieldworkmeasurementdict'))
-
-        self._widget = None
-        self.measurements = None
-        self.model = None
-
-    def configure(self):
-        d = ConfigureDialog(self._state)
-        d.setModal(True)
-        if d.exec_():
-            self._state = d.getState()
-            self.serialize(self._location)
-            
-        self._configured = d.validate()
-        if self._configured and self._configuredObserver:
-            self._configuredObserver()
-
-    def execute(self):
-
-        self.measurements = femur_measurements.FemurMeasurements( self.model )
-        self.measurements.calcMeasurements()
-        
-        # if self._state._verbose:
-        self.measurements.printMeasurements()
-            
-        # return m
-        print 'measurements done'
-        self._doneExecution()
-
-    def setPortData(self, index, dataIn):
-        if not isinstance(dataIn, geometric_field.geometric_field):
-            raise TypeError, 'FieldViViewFieldworkModelStep expects a geometric_field as input'
-        
-        self.model = dataIn
-
-    def getPortData(self, index):
-        print 'outputting from FieldworkFemurMeasurementStep'
-        return {'femur measurements':self.measurements}
-    
-    def getIdentifier(self):
-        return self._state._identifier
-     
-    def setIdentifier(self, identifier):
-        self._state._identifier = identifier
-     
-    def serialize(self, location):
-        configuration_file = os.path.join(location, getConfigFilename(self._state._identifier))
-        s = QtCore.QSettings(configuration_file, QtCore.QSettings.IniFormat)
-        s.beginGroup('state')
-        s.setValue('identifier', self._state._identifier)
-        s.setValue('verbose', self._state._verbose)
-        s.endGroup()
-     
-    def deserialize(self, location):
-        configuration_file = os.path.join(location, getConfigFilename(self._state._identifier))
-        s = QtCore.QSettings(configuration_file, QtCore.QSettings.IniFormat)
-        s.beginGroup('state')
-        self._state._identifier = s.value('identifier', '')
-        if s.value('verbose', '')=='True':
-            self._state._verbose = True
-        else:
-            self._state._verbose = False
-        s.endGroup()
-        d = ConfigureDialog(self._state)
-        self._configured = d.validate()
-     
-def getConfigFilename(identifier):
-    return identifier + '.conf'
-=======
 '''
 MAP Client, a program to generate detailed musculoskeletal models for OpenSim.
     Copyright (C) 2012  University of Auckland
@@ -141,8 +25,8 @@ from PySide import QtGui
 from mapclient.mountpoints.workflowstep import WorkflowStepMountPoint
 
 from mapclientplugins.fieldworkfemurmeasurementstep.widgets.configuredialog import ConfigureDialog
-from gias.musculoskeletal import fw_femur_measurements
-from fieldwork.field import geometric_field
+from gias2.musculoskeletal import fw_femur_measurements
+from gias2.fieldwork.field import geometric_field
 
 class FieldworkFemurMeasurementStep(WorkflowStepMountPoint):
     '''
@@ -228,5 +112,3 @@ class FieldworkFemurMeasurementStep(WorkflowStepMountPoint):
         d.identifierOccursCount = self._identifierOccursCount
         d.setConfig(self._config)
         self._configured = d.validate()
-
->>>>>>> e2f5189dd5b53c8ea71632e92387d00eeebe2c27
